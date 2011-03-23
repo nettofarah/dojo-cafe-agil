7b56b6b6d675b9309a5b8be0755aecc6bbefbefe
Netto <nettofarah@gmail.com>
2011-03-22 23:18:48 -0300

    adding some stuff

--- spec/poker_hand_suite.js
+++ spec/poker_hand_suite.js
@@ -1,12 +1,63 @@
 describe('PokerHand', function () {
 	
-	describe('ranking a card hand',function(){
-
-		it('should rank high cards as 1',function(){
-			var cardHand = CardHand('2H 3D 5S 9C KD');
-			expect(cardHand.score()).toEqual(1);
+	describe('deciding a winner',function(){
+		it("should evaluate high scores", function() {
+		  	var white = CardHand('2H 7D 5C 9S AK');
+			var black = CardHand('2H 7D 5C 9S QC');
+			expect(white.score()).toBeGreaterThan(black.score());
 		});
-
+		
+		it("should evaluate pair scores", function() {
+		  	var white = CardHand('2H 2C 5S 9S QK')
+			var black = CardHand('2H 7D 5C 9S AC');
+			expect(white.score()).toBeGreaterThan(black.score());
+		});
 	});
 	
+	describe('ranking a card hand',function(){
+		it('should rank high cards as the higher value',function(){
+			var cardHand = CardHand('2H 7D 5S 9C');
+			expect(cardHand.score()).toEqual(9);
+		});
+	});
+	
+	describe('parsing a card hand',function(){
+		it('should parse single value cards',function(){
+			var cardHand = CardHand('2H 3D');
+			var twoHearts = Card('2','H');
+			var threeDiamonds = Card('3','D');
+			
+			expect(cardHand.cards()[0]).toEqual(twoHearts);
+			expect(cardHand.cards()[1]).toEqual(threeDiamonds);
+		});
+		
+		it('should parse a "char" card',function(){
+			var cardHand = CardHand('TC KD JH QS AC');
+			var cards = [Card('T','C'),Card('K','D'), Card('J','H'), Card('Q','S'), Card('A','C')];
+			for(c in cards){
+				expect(cardHand.cards()).toContain(cards[c]);
+			}
+		});
+	});
+	
+});
+
+describe("Card", function() {
+  	it('should have a number value',function() {
+		var two = Card('2','H');
+		expect(two.number_value).toEqual(2);
+	});
+	
+	it('should have a number value for char cards',function(){
+		expected_values = {
+			'T' : 10,
+			'J' : 11,
+			'Q' : 12,
+			'K' : 13,
+			'A' : 14
+		}
+		for (key in expected_values){
+			expect(Card(key,'D').number_value).toEqual(expected_values[key]);	
+		}
+	});
 });
--- src/card_hand.js
+++ src/card_hand.js
@@ -1,9 +1,76 @@
-var CardHand = function(){
-	var self = {};
+var CardHand = function(raw_cards){
+	var api = {};
+	var cards = [];
 	
-	self.score = function(){
-		return 1;
+	var MAX_HIGH = 14;
+	
+	var parse = function(){
+		var splited_cards = raw_cards.split(' ');
+		for(raw_index in splited_cards){
+			var card = extract_card(splited_cards[raw_index]);
+			cards.push(card);
+		}
 	}
 	
-	return self;
+	var extract_card = function(card_info){
+		var card_info = card_info.split('');
+		var value = card_info[0];
+		var suit = card_info[1];
+		return Card(value,suit);
+	}
+	
+	parse();
+	
+	var high = function(){
+		var higher = cards[0];
+		for(c in cards){
+			if (cards[c].number_value > higher.number_value){
+				higher = cards[c];
+			}
+		}
+		return higher.number_value;
+	}
+	
+	var pair = function(){
+		for(var i=0; i<cards.length; i++){
+			for(var j=i+1; j<cards.length; j++){
+				if (cards[i].number_value == cards[j].number_value) return cards[i].number_value;
+			}
+		}
+		return 0;
+	}
+	
+	api.score = function(){
+		var pair_value = pair();
+		if (pair_value != 0) return pair_value + MAX_HIGH;
+		return high();
+	}
+	
+	api.cards = function(){
+		return cards;
+	}
+	
+	return api;
+}
+
+var Card = function(value, suit){
+	
+	var value_to_number = {
+		'T' : 10,
+		'J' : 11,
+		'Q' : 12,
+		'K' : 13,
+		'A' : 14
+	}
+	
+	var number_value = function(){
+		var int_value = parseInt(value);
+		return int_value ? int_value : value_to_number[value];
+	}
+	
+	return {
+		value : value,
+		suit : suit,
+		number_value : number_value()
+	}
 }
